9288c1cdeb9099b701eb394d9a662166d5b29112
José A. Puértolas <63794670+jose-nferx@users.noreply.github.com>
2020-09-20 14:33:48 +0200

    Bump version: 0.1.52 → 0.1.53

--- bashrc/scripts/bashrc-install-passwd.sh
+++ bashrc/scripts/bashrc-install-passwd.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 export starting="${BASH_SOURCE[0]}"; debug.sh starting
 
+test -n "${PASSWD_PATH}" || { error.sh PASSWD_PATH 'not defined'; return 1; }
+debug.sh PASSWD_PATH USERHOME
+
 unset starting
--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
     setup_requires=[],
     tests_require=[],
     url=f'{GITHUB_URL}/',
-    version='0.1.52',
+    version='0.1.53',
     zip_safe=False,
 )
