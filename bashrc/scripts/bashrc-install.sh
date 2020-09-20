2f73244bdf2d5b650a0caabe32742de5978186ab
José A. Puértolas <63794670+jose-nferx@users.noreply.github.com>
2020-09-20 14:52:50 +0200

    Bump version: 0.1.55 → 0.1.56

--- bashrc/scripts/bashrc-install.sh
+++ bashrc/scripts/bashrc-install.sh
@@ -1,6 +1,45 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
+# "${1}" - ACCOUNT PASSWORD
+# "${2}" - INTERNET PASSWORD
 export starting="${BASH_SOURCE[0]}"; debug.sh starting
+
+function paswwd() {
+  # "${1}" - ACCOUNT PASSWORD
+  # "${2}" - INTERNET PASSWORD
+  local PASSWORD INTERNET GITHUB_PRIVATE_URL GITHUB_SECRETS_URL
+  test -n "${PASSWD_PATH}" || { error.sh PASSWD_PATH 'not defined'; return 1; }
+  test -n "${GITHUB_USERNAME}" || { error.sh GITHUB_USERNAME 'not defined'; return 1; }
+
+  if ! test -f "${PASSWD_PATH}"; then
+    PASSWORD="${1}"
+    if ! test -n "${PASSWORD}"; then
+      read -r -p "$( blue.sh "Enter sudo password: " )" PASSWORD
+    fi
+    if test -n "${DARWIN}"; then
+      INTERNET="${2}"
+      if ! test -n "${INTERNET}"; then
+        read -r -p "$( blue.sh "Enter internet/GitHub password: " )" INTERNET
+      fi
+    else
+      INTERNET="${PASSWORD}"
+    fi
+    if echo "${PASSWORD}" | sudo -S true; then
+      info.sh passwd "sudo password."
+    else
+      error.sh passwd "sudo password."
+      return 1
+    fi
+  GITHUB_PRIVATE_URL="https://${GITHUB_USERNAME}:${INTERNET}@github.com/${GITHUB_USERNAME}"
+  # shellcheck disable=SC2034
+  GITHUB_SECRETS_URL="${GITHUB_PRIVATE_URL}/.ssh"
+  for var in PASSWORD INTERNET GITHUB_PRIVATE_URL GITHUB_SECRETS_URL; do
+    echo "export ${var}='${!var}'" | tee -a "${PASSWD_PATH}" > /dev/null 2>&1
+  done
+    debug.sh PASSWD_PATH GITHUB_USERNAME PASSWORD INTERNET GITHUB_PRIVATE_URL GITHUB_SECRETS_URL
+    info.sh "${PASSWD_PATH}" created
+  fi
+}
 
 ## BEGIN: INIT
 source_name="bashrc-init"
@@ -14,13 +53,7 @@ fi
 ## END: INIT
 
 ## BEGIN: PASSWD
-script_name="bashrc-install-passwd.sh"
-script_path="$( command -v "${script_name}" )"; export script_path; debug.sh script_path
-if test -f "${script_path}"; then
-  "${script_path}"
-else
-  error.sh install "${script_name}" "not found"; exit 1
-fi
+! passwd "$@" || exit 1
 
 if test -f "${PASSWD_PATH}"; then
   source "${PASSWD_PATH}"
--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
     setup_requires=[],
     tests_require=[],
     url=f'{GITHUB_URL}/',
-    version='0.1.55',
+    version='0.1.56',
     zip_safe=False,
 )
