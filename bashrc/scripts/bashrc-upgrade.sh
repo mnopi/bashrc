e4bb833658eb859e9564551ad29ef487b229f3ba
j5pux <63794670+j5pux@users.noreply.github.com>
2020-09-21 08:57:05 +0200

    Bump version: 0.1.143 → 0.1.144

--- bashrc/scripts/bashrc-upgrade.sh
+++ bashrc/scripts/bashrc-upgrade.sh
@@ -2,9 +2,11 @@
 # shellcheck disable=SC2034
 export starting="${BASH_SOURCE[0]}"; debug.sh starting
 
+test -n "${PROJECT_BASHRC}" || { error.sh "PROJECT_BASHRC" "empty"; exit 1; }
+
 unset VIRTUAL_ENV PYTHONHOME
 deactivate > /dev/null 2>&1
 
-project-upgrade.sh "$( basename "${PROJECT_BASHRC}" )"  || exit 1
+project-upgrade.sh "${PROJECT_BASHRC}" || exit 1
 
 unset starting
