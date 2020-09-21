9de6ec8447ef3258d864c13123fec01866550fe6
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2020-09-21 08:57:05 +0200

    Bump version: 0.1.143 → 0.1.144

--- bashrc/scripts/project-upgrade.sh
+++ bashrc/scripts/project-upgrade.sh
@@ -3,18 +3,24 @@
 # shellcheck disable=SC2034
 export starting="${BASH_SOURCE[0]}"; debug.sh starting
 
+path="${1:-.}"
+
+cd "${path}" > /dev/null 2>&1 || { error.sh "${path}" "invalid"; exit 1; }
+
+path="$( pwd )"
+name="$( basename "${path}" )"
+export path name; debug.sh path name
+
 unset VIRTUAL_ENV PYTHONHOME
 deactivate > /dev/null 2>&1
 
-test -n "${DARWIN}" || sudo=sudo
-echo $name
-if error="$( /usr/local/bin/pip3.8 install --upgrade "${1}" 2>&1 )"; then
-  info.sh upgrade "${1}"
+if error="$( /usr/local/bin/pip3.8 install -vvvv --upgrade "${name}" 2>&1 )"; then
+  info.sh upgrade "${name}"
 else
-  error.sh upgrade "${1}" "${error}"; exit 1
+  error.sh upgrade "${name}" "${error}"; exit 1
 fi
 
 ## I do not know why requires twice to get it into the path,
-${sudo} /usr/local/bin/pip3.8 install --upgrade "${1}" > /dev/null 2>&1
+/usr/local/bin/pip3.8 install --upgrade "${name}" > /dev/null 2>&1
 
 unset starting error path name
