da75eeb822aa123f32ea577d6c726402df050a64
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2020-09-21 09:45:06 +0200

    Bump version: 0.1.153 → 0.1.154

--- bashrc/scripts/project-upgrade.sh
+++ bashrc/scripts/project-upgrade.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC2034
 export starting="${BASH_SOURCE[0]}"; debug.sh starting
 
-name="$( basename pwd )"
+name="$( basename "$( pwd )" )"
 
 unset VIRTUAL_ENV PYTHONHOME
 deactivate > /dev/null 2>&1
@@ -13,17 +13,12 @@ if ! test -n "${DARWIN}"; then
   command="sudo /bin/python3.8"
 fi
 
+name="${1:-${name}}"
 
-if error="$( ${command} -m pip uninstall -y "${1:-${name}}" 2>&1 )"; then
-  info.sh remove "${1:-${name}}"
+if error="$( ${command} -m pip install --upgrade "${name}" 2>&1 )"; then
+  info.sh upgrade "${name}"
 else
-  error.sh remove "${1:-${name}}" "${error}"; exit 1
-fi
-
-if error="$( ${command} -m pip install --upgrade "${1:-${name}}" 2>&1 )"; then
-  info.sh install "${1:-${name}}"
-else
-  error.sh install "${1:-${name}}" "${error}"; exit 1
+  error.sh upgrade "${name}" "${error}"; exit 1
 fi
 
 unset starting error command
