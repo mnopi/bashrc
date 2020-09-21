342f63f2c250181af8f9141221d190a059398e35
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2020-09-21 05:49:55 +0200

    Bump version: 0.1.108 → 0.1.109

--- bashrc/scripts/sshconfig.sh
+++ bashrc/scripts/sshconfig.sh
@@ -5,6 +5,7 @@ set -x
 if test "${USER}" = "${USERNAME}" && isuserdarwin.sh && test -n "${1}"; then
   bashrc-upload.sh || exit 1
   bashrc-upgrade.sh || exit 1
+  echo hola
   source ~/.bashrc
 fi
 
--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
     setup_requires=[],
     tests_require=[],
     url=f'{GITHUB_URL}/',
-    version='0.1.108',
+    version='0.1.109',
     zip_safe=False,
 )
