6d6cb66d2273820b0f03fe81c5ccb25243896cb3
j5pux <63794670+j5pux@users.noreply.github.com>
2020-09-21 13:52:42 +0200

    Bump version: 0.1.161 → 0.1.162

--- bashrc/scripts/sudoers.sh
+++ bashrc/scripts/sudoers.sh
@@ -82,13 +82,13 @@ Defaults !requiretty
 ${user} ALL=(ALL) NOPASSWD:ALL
 Defaults: ${user} !logfile, !syslog
 EOT
-        if error="$( sudo /usr/sbin/visudo -cf "${file}" 2>&1 )"; then
-          info.sh sudoers "${file}"
-        else
-          if ! echo "${error}" | grep "Permission denied" > /dev/null 2>&1; then
-            error.sh sudoers 'visudo -cf' "${file}"; return 1
-          fi
-        fi
+#        if error="$( sudo /usr/sbin/visudo -cf "${file}" 2>&1 )"; then
+#          info.sh sudoers "${file}"
+#        else
+#          if ! echo "${error}" | grep "Permission denied" > /dev/null 2>&1; then
+#            error.sh sudoers 'visudo -cf' "${file}"; return 1
+#          fi
+#        fi
       else
         error.sh sudoers tee "${file}"; return 1
       fi
