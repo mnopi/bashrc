0b6eb2a626ba8e1504ae6c59de9035f534d1a75d
j5pux <63794670+j5pux@users.noreply.github.com>
2020-09-21 07:21:30 +0200

    Bump version: 0.1.126 → 0.1.127

--- bashrc/scripts/gpull.sh
+++ bashrc/scripts/gpull.sh
@@ -6,22 +6,26 @@ if ! isuser.sh; then
   error.sh "can not be done with root"; exit 1
 fi
 
-if name="$( source gtop 2>&1 )"; then
-  export name; debug.sh name
-  path="$( git rev-parse --show-toplevel 2>&1 )"; export path; debug.sh path
+if path="$( git rev-parse --show-toplevel 2>&1 )"; then
   cd "${path}" || { error.sh cd "${path}"; exit 1; }
-    if error="$( git pull --no-rebase --quiet --no-stat 2>&1 )"; then
+  name="$( basename "${path}" )"
+else
+  error.sh "${BASH_SOURCE[0]}" "not a git dir"; exit 1
+fi
+
+export name; debug.sh name
+path="$( git rev-parse --show-toplevel 2>&1 )"; export path; debug.sh path
+cd "${path}" || { error.sh cd "${path}"; exit 1; }
+  if error="$( git pull --no-rebase --quiet --no-stat 2>&1 )"; then
+    info.sh pull "${name}"; exit 0
+  else
+    if error1="$( git pull --no-rebase --quiet --no-stat origin master 2>&1 )"; then
       info.sh pull "${name}"; exit 0
     else
-      if error1="$( git pull --no-rebase --quiet --no-stat origin master 2>&1 )"; then
-        info.sh pull "${name}"; exit 0
-      else
-        error.sh "pull ${name}" "${error}\n ${error1}"; exit 1
-      fi
+      error.sh "pull ${name}" "${error}\n ${error1}"; exit 1
     fi
-  cd - > /dev/null || exit 1
-else
-  error.sh gall "${name}"; exit 1
-fi
+  fi
+cd - > /dev/null || exit 1
+
 
 unset starting name path error error1
