105590d20309e44b669738c052400ac924618ee0
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2020-09-21 05:53:31 +0200

    Bump version: 0.1.111 → 0.1.112

--- bashrc/scripts/bashrc-upgrade.sh
+++ bashrc/scripts/bashrc-upgrade.sh
@@ -7,6 +7,6 @@ test -n "${PROJECT_BASHRC}" || { error.sh "PROJECT_BASHRC" "empty"; exit 1; }
 unset VIRTUAL_ENV PYTHONHOME
 deactivate > /dev/null 2>&1
 
-project-upgrade.sh "$( basename "${PROJECT_BASHRC}" )" || exit 1
+project-upgrade.sh "${PROJECT_BASHRC}" || exit 1
 
 unset starting
--- bashrc/scripts/project-upgrade.sh
+++ bashrc/scripts/project-upgrade.sh
@@ -2,7 +2,7 @@
 # ${1} - name
 # shellcheck disable=SC2034
 export starting="${BASH_SOURCE[0]}"; debug.sh starting
-set -x
+
 path="${1:-.}"
 
 cd "${path}" > /dev/null 2>&1 || { error.sh "${path}" "invalid"; exit 1; }
@@ -22,5 +22,5 @@ fi
 
 ## I do not know why requires twice to get it into the path,
 /usr/local/bin/pip3.8 install --upgrade "${name}" > /dev/null 2>&1
-set +x
+
 unset starting error path name
