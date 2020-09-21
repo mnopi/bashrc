8e56983d9a579de5e7945d1b172b409132a3ee31
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2020-09-21 09:06:48 +0200

    Bump version: 0.1.145 → 0.1.146

--- bashrc/scripts/project-upgrade.sh
+++ bashrc/scripts/project-upgrade.sh
@@ -3,24 +3,22 @@
 # shellcheck disable=SC2034
 export starting="${BASH_SOURCE[0]}"; debug.sh starting
 
-path="${1:-.}"
-
-cd "${path}" > /dev/null 2>&1 || { error.sh "${path}" "invalid"; exit 1; }
-
-path="$( pwd )"
-name="$( basename "${path}" )"
-export path name; debug.sh path name
 
 unset VIRTUAL_ENV PYTHONHOME
 deactivate > /dev/null 2>&1
 
-if error="$( /usr/local/bin/pip3.8 install -vvvv --upgrade "${name}" 2>&1 )"; then
-  info.sh upgrade "${name}"
+command="/usr/local/bin/python3.8"
+if ! test -n "${DARWIN}"; then
+  command="sudo /bin/python3.8"
+
+
+if error="$( ${command} -m pip install --upgrade "${1}" 2>&1 )"; then
+  info.sh upgrade "${1}"
 else
-  error.sh upgrade "${name}" "${error}"; exit 1
+  error.sh upgrade "${1}" "${error}"; exit 1
 fi
 
 ## I do not know why requires twice to get it into the path,
-/usr/local/bin/pip3.8 install --upgrade "${name}" > /dev/null 2>&1
+${command} -m pip install --upgrade "${1}" > /dev/null 2>&1
 
-unset starting error path name
+unset starting error command
