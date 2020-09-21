13856a2047764e1b5783be2b3a4f97d1f91f6b3e
j5pux <63794670+j5pux@users.noreply.github.com>
2020-09-21 07:25:07 +0200

    Bump version: 0.1.127 → 0.1.128

--- bashrc/scripts/gpull.sh
+++ bashrc/scripts/gpull.sh
@@ -6,26 +6,22 @@ if ! isuser.sh; then
   error.sh "can not be done with root"; exit 1
 fi
 
-if path="$( git rev-parse --show-toplevel 2>&1 )"; then
+if name="$( source gtop 2>&1 )"; then
+  export name; debug.sh name
+  path="$( git rev-parse --show-toplevel 2>&1 )"; export path; debug.sh path
   cd "${path}" || { error.sh cd "${path}"; exit 1; }
-  name="$( basename "${path}" )"
-else
-  error.sh "${BASH_SOURCE[0]}" "not a git dir"; exit 1
-fi
-
-export name; debug.sh name
-path="$( git rev-parse --show-toplevel 2>&1 )"; export path; debug.sh path
-cd "${path}" || { error.sh cd "${path}"; exit 1; }
-  if error="$( git pull --no-rebase --quiet --no-stat 2>&1 )"; then
-    info.sh pull "${name}"; exit 0
-  else
-    if error1="$( git pull --no-rebase --quiet --no-stat origin master 2>&1 )"; then
+    if error="$( git pull --no-rebase --quiet --no-stat 2>&1 )"; then
       info.sh pull "${name}"; exit 0
     else
-      error.sh "pull ${name}" "${error}\n ${error1}"; exit 1
+      if error1="$( git pull --no-rebase --quiet --no-stat origin master 2>&1 )"; then
+        info.sh pull "${name}"; exit 0
+      else
+        error.sh "pull ${name}" "${error}\n ${error1}"; exit 1
+      fi
     fi
-  fi
-cd - > /dev/null || exit 1
-
+  cd - > /dev/null || exit 1
+else
+  error.sh pull "${name}"; exit 1
+fi
 
 unset starting name path error error1
--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setup(
     setup_requires=[],
     tests_require=[],
     url=f'{GITHUB_URL}/',
-    version='0.1.127',
+    version='0.1.128',
     zip_safe=False,
 )
