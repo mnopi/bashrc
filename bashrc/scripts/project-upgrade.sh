db2ca1d4c1ea7bf80ed138b03b8b2f4fc209021d
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2020-09-23 13:44:33 +0200

    Bump version: 0.31.19 → 0.31.20

--- bashrc/scripts/project-upgrade.sh
+++ bashrc/scripts/project-upgrade.sh
@@ -8,14 +8,19 @@ deactivate > /dev/null 2>&1
 
 basename="$( basename "$( pwd )" )"
 name="${1:-${basename}}"
-[[ "${name}" != "${PEN_BASENAME}" ]]  || name="${PEN_GIT}"
+url="${name}"
+if [[ "${name}" == "${PEN_BASENAME}" ]]; then
+  url="${PEN_GIT}"
+  name="${PEN_BASENAME}"
+fi
 
 command="/usr/local/bin/python3.8"
 if ! test -n "${DARWIN}"; then
   command="sudo /bin/python3.8"
 fi
 
-if error="$( ${command} -m pip uninstall "${name}" -y 2>&1 && ${command} -m pip install --upgrade "${name}" 2>&1 )"; then
+
+if error="$( ${command} -m pip uninstall "${name}" -y 2>&1 && ${command} -m pip install --upgrade "${url}" 2>&1 )"; then
   info.sh install "${name}"
 else
   error.sh install "${name}" "${error}"; exit 1
