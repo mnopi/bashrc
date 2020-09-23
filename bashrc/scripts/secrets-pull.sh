5fc14e3bf337aef222a671e0e26a45f5a3c34f5b
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2020-09-23 13:25:19 +0200

    Bump version: 0.31.16 → 0.31.17

--- bashrc/scripts/secrets-pull.sh
+++ bashrc/scripts/secrets-pull.sh
@@ -2,8 +2,11 @@
 # shellcheck disable=SC2034
 export starting="${BASH_SOURCE[0]}"; debug.sh starting
 
-test -n "${GITHUB_SECRETS_PATH}" || { error.sh GITHUB_SECRETS_PATH 'not defined'; exit 1; }
-cd "${GITHUB_SECRETS_PATH}" || { error.sh "${GITHUB_SECRETS_PATH}" 'not found'; exit 1; }
-gpull.sh
-cd - > /dev/null || exit 1
+if isuserdarwin.sh; then
+  test -n "${GITHUB_SECRETS_PATH}" || { error.sh GITHUB_SECRETS_PATH 'not defined'; exit 1; }
+  cd "${GITHUB_SECRETS_PATH}" || { error.sh "${GITHUB_SECRETS_PATH}" 'not found'; exit 1; }
+  sudo chown -R "${USERNAME}":"$( id -g "${USERNAME}" )" "${USERNAME}/.ssh"
+  gpull.sh
+  cd - > /dev/null || exit 1
+fi
 unset starting
