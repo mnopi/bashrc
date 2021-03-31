08b93c6b36f85663e14ebefc589fc0e42d953efd
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2021-03-31 02:10:14 +0200

    'auto commit'

--- rc/__init__.py
+++ rc/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 """Bashrc Package."""
+
+__all__ = ['package', 'project', 'scripts', 'scripts_relative', 'readme', 'description']
--- setup.py
+++ setup.py
@@ -4,28 +4,48 @@
 from setuptools import setup, find_packages
 
 # noinspection PyUnresolvedReferences
-from bashrc import package, project, scripts_relative, readme, description, install_requires, GITHUB_EMAIL, \
-    REALNAME, GITHUB_URL, command
+import os
+import pathlib
+
+
+project = pathlib.Path(__file__).resolve()
+
+scripts = project / 'scripts'
+scripts_relative = [str(item.relative_to(project)) for item in scripts.iterdir()]
+#
+# readme = project / 'README.md'
+# description = package.name
+# if readme.is_file():
+#     try:
+#         description = str(readme).splitlines()[0].split('#')[1]
+#     except IndexError:
+#         pass
+
+requirements = project / 'requirements.txt'
+if requirements.is_file():
+    install_requires = requirements.read_text().splitlines()
+else:
+    install_requires = list()
 
 setup(
     # author=REALNAME,
     # author_email=GITHUB_EMAIL,
     # description=description,
-    entry_points={
-        'console_scripts': [
-            f'{command} = {package.name}:app',
-        ],
-    },
-    include_package_data=True,
+    # entry_points={
+    #     'console_scripts': [
+    #         f'{command} = {package.name}:app',
+    #     ],
+    # },
+    # include_package_data=True,
     install_requires=install_requires,
-    name=package.name,
-    package_data={
-        project.name: [f'{project.name}/scripts/*'],
-    },
-    packages=find_packages(include=[package.name]),
+    name=project.name,
+    # package_data={
+    #     project.name: [f'{project.name}/scripts/*'],
+    # },
+    packages='rc',
     scripts=scripts_relative,
-    setup_requires=[],
-    tests_require=[],
+    # setup_requires=[],
+    # tests_require=[],
     # url=f'{GITHUB_URL}/',
     version='0.31.55',
     zip_safe=False,
