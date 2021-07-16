e488bf61f8cce04dbf62cbb61f026a3b2a5ec5c3
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2021-07-16 07:51:29 +0200

    'auto commit'

--- scripts/ghconfig.sh
+++ scripts/ghconfig.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-export starting="${BASH_SOURCE[0]}"; debug.sh starting
-[[ ! "${TIMES-}" ]] || start="$( date +%s )"
+export starting="${BASH_SOURCE[0]}"
+debug.sh starting
+[[ ! "${TIMES-}" ]] || start="$(date +%s)"
 
 # shellcheck disable=SC2016
 gh alias set loguser --shell 'gh auth login --with-token <<< $GITHUB_USER_TOKEN'
 # shellcheck disable=SC2016
 gh alias set logwork --shell 'gh auth login --with-token <<< $GITHUB_WORK_TOKEN'
+# shellcheck disable=SC2016
+gh alias set clone 'repo clone "$1" -- --quiet' # gh clonesilent j5pu/data
 
 info.sh ghconfig
 cd "${USERHOME}/git" || exit 1
 gall.sh
 if secrets.sh; then exit 0; else exit 1; fi
 
-[[ ! "${TIMES-}" ]] || { end="$( date +%s )"; export runtime=$((end-start)); times.sh starting runtime; }
+[[ ! "${TIMES-}" ]] || {
+  end="$(date +%s)"
+  export runtime=$((end - start))
+  times.sh starting runtime
+}
 unset start end runtime starting
