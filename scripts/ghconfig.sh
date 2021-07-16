011979877c7b9b6be673dcca6313ceb3f2bf51f6
jose-nferx <63794670+jose-nferx@users.noreply.github.com>
2021-07-16 07:30:46 +0200

    'auto commit'

---
+++ scripts/ghconfig.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+export starting="${BASH_SOURCE[0]}"; debug.sh starting
+[[ ! "${TIMES-}" ]] || start="$( date +%s )"
+
+# shellcheck disable=SC2016
+gh alias set loguser --shell 'gh auth login --with-token <<< $GITHUB_USER_TOKEN'
+# shellcheck disable=SC2016
+gh alias set logwork --shell 'gh auth login --with-token <<< $GITHUB_WORK_TOKEN'
+
+info.sh ghconfig
+cd "${USERHOME/git}" || exit 1
+gall.sh
+if secrets.sh; then exit 0; else exit 1; fi
+
+[[ ! "${TIMES-}" ]] || { end="$( date +%s )"; export runtime=$((end-start)); times.sh starting runtime; }
+unset start end runtime starting
